2eab73347f056d53c8e49c6ea0edbd68ad70792d
Nivesh Bansal <162882605+niveshbansal07@users.noreply.github.com>
2026-02-12 11:42:43 +0530

    Change database URI environment variable
    Updated SQLALCHEMY_DATABASE_URI to use DATABASE_URL.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DB_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_TOKEN_LOCATION = ["cookies"]
@@ -13,4 +13,5 @@ class Config:
     JWT_COOKIE_SECURE = False        # local ke liye
     JWT_COOKIE_SAMESITE = "Lax"      # VERY IMPORTANT
     JWT_COOKIE_CSRF_PROTECT = False
+
 
