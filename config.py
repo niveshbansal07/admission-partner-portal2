9dda46d4054a5aa9c2c11fa0946bf92155d9e9ab
Nivesh Bansal <162882605+niveshbansal07@users.noreply.github.com>
2026-02-12 11:17:41 +0530

    Refactor database URI to use environment variable

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:0522nivesh#@localhost/admission"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DB_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_TOKEN_LOCATION = ["cookies"]
@@ -13,3 +13,4 @@ class Config:
     JWT_COOKIE_SECURE = False        # local ke liye
     JWT_COOKIE_SAMESITE = "Lax"      # VERY IMPORTANT
     JWT_COOKIE_CSRF_PROTECT = False
+
